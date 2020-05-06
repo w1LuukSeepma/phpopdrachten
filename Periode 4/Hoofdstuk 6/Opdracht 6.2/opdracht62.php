e70e6e08c1c199f07d93b9232c6b7d52f36bc134
Luuk <luuk.seepma@edu-kw1c.nl>
2020-05-06 15:13:53 +0200

    Opdracht 6.2

---
+++ Periode 4/Hoofdstuk 6/Opdracht 6.2/opdracht62.php
@@ -0,0 +1,78 @@
+<div id="main">
+    <form id="gameFrm" method="get" action="opdracht62.php">
+         <div class="float">
+             <input type="radio" onchange="document.getElementById('gameFrm').submit();" name="keuze" value="steen" id="steen">
+             <label for="steen">Steen</label>
+         </div>
+         <div class="float">
+             <input type="radio" onchange="document.getElementById('gameFrm').submit();" name="keuze" value="papier" id="papier">
+             <label for="papier">Papier</label>
+         </div>
+         <div class="float">
+             <input type="radio" onchange="document.getElementById('gameFrm').submit();" name="keuze" value="schaar" id="schaar">
+             <label for="schaar">Schaar</label>
+         </div>
+    </form>
+</div>
+<?php
+session_start();
+//Spelregels
+/*
+* gelijke keuzes wint niemand
+* schaar wint van papier
+* schaar verliest van steen
+* steen wint van schaar
+* steen verliest van papier
+* papier wint van steen steen
+* papier verliest schaar
+*/
+
+if(!isset($_SESSION['user1']) && !isset($_SESSION['user2']))
+{
+    $_SESSION['user1'] = 0;
+    $_SESSION['user2'] = 0;
+}
+
+if (isset($_GET['keuze']))
+{
+    echo "Jij koos: " . $_GET['keuze'] . "<br>";
+
+    $opties = array("steen","papier","schaar");
+
+    $computerkeuzegetal = rand(0,2);
+    $computerkeuze = $opties[$computerkeuzegetal];
+    echo "De computer koos: " . $computerkeuze;
+
+    if ($_GET['keuze'] == "steen" && $computerkeuze == "schaar" || $_GET['keuze'] == "papier" && $computerkeuze == "steen" || $_GET['keuze'] == "schaar" && $computerkeuze == "papier" )
+    {
+        $_SESSION['user1'] += 1;
+        $_SESSION['user2'] += 0;
+    }
+    elseif ($computerkeuze == "steen" && $_GET['keuze'] == "schaar" || $computerkeuze == "papier" && $_GET['keuze'] == "steen" || $computerkeuze == "schaar" && $_GET['keuze'] == "papier")
+    {
+        $_SESSION['user1'] += 0;
+        $_SESSION['user2'] += 1;
+    }
+    elseif ($computerkeuze == "steen" && $_GET['keuze'] == "steen" || $computerkeuze == "papier" && $_GET['keuze'] == "papier" || $computerkeuze == "schaar" && $_GET['keuze'] == "schaar")
+    {
+        $_SESSION['user1'] += 0;
+        $_SESSION['user2'] += 0;
+    }
+
+    echo "<p>" . $_SESSION['user1'] . "</p>";
+    echo "<p>" . $_SESSION['user2'] . "</p>";
+
+    if($_SESSION['user1'] == 5)
+    {
+        echo "<p class='pos'>" . "Je hebt gewonnen knaap" . "</p>";
+        session_destroy();
+    }
+    elseif($_SESSION['user2'] == 5)
+    {
+        echo "<p class='pos'>" . "Je bent een lozer knaap" . "</p>";
+        session_destroy();
+    }
+
+}
+
+
